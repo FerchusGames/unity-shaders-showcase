18b59f6874984fea750e0ed3df939c63f06fad08
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2023-05-13 23:41:28 -0600

    Finished Exam

--- Shaders/Assets/Scenes/Examen/SpaceRoutine.cs
+++ Shaders/Assets/Scenes/Examen/SpaceRoutine.cs
@@ -1,108 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class SpaceRoutine : MonoBehaviour
-{
+public class SpaceRoutine : MonoBehaviour {
 
-    public Renderer meshRenderer = default;
-    Color current = default;
-    float h = 0;
-    bool isToon = false;
-    private void Awake()
-    {
-        if (meshRenderer == null)
-            meshRenderer = GetComponent<Renderer>();
-
-        current = Color.HSVToRGB(0, 1, 1);
-        isToon = meshRenderer.sharedMaterial.GetFloat("_UseToon")>0?true:false;
-        //meshRenderer.sharedMaterial.SetColor()
+    [SerializeField] Renderer _meshRenderer = default;
+    Color _currentColor = default;
+    
+    float _hue = 0;
+    
+    bool _isToon = false;
+    bool _isTransparent = false;
+    bool _isVisible = false;
+    
+    bool _canTransition = true;
+    
+    private void Awake() {
+        if (_meshRenderer == null)
+            _meshRenderer = GetComponent<Renderer>();
+        
+        _currentColor = Color.HSVToRGB(0, 1, 1);
+        _isToon = _meshRenderer.sharedMaterial.GetFloat("_PBR_Toon_Lerp") > 0;
+        _isTransparent = _meshRenderer.sharedMaterial.GetFloat("_Transition_Value") > 0;
+        _isVisible = _meshRenderer.sharedMaterial.GetFloat("_Visibility") > 0;
     }
 
-    private void Update()
-    {
-        h += Time.deltaTime*0.1f;
-        if (h >= 1) h = 0;
-        current = Color.HSVToRGB(h, 1, 1);
-        meshRenderer.sharedMaterial.SetColor("_MainColor", current);
+    private void Update() {
+        ChangeHue();
     }
 
-    [ContextMenu("Aparecer")]
-    public void EnableTransition()
-    {
-        StartCoroutine(DoTransition(2.5f, true));
+    private void ChangeHue() {
+        _hue += Time.deltaTime*0.1f;
+        if (_hue >= 1) _hue = 0;
+        _currentColor = Color.HSVToRGB(_hue, 1, 1);
+        _meshRenderer.sharedMaterial.SetColor("_Base_Color", _currentColor);
     }
 
-    [ContextMenu("Desaparecer")]
-    public void DisableTransition()
-    {
-        StartCoroutine(DoTransition(2.5f, false));
+    [ContextMenu("PBR Toon Switch")]
+    public void PBRToonSwitch() {
+        Transition(ref _isToon, "_PBR_Toon_Lerp");
+    }
+    
+    [ContextMenu("Transition")]
+    public void OpaqueTransparentSwitch() {
+        Transition(ref _isTransparent, "_Transition_Value");
     }
 
-    [ContextMenu("Transparent")]
-    public void DoTransparency()
-    {
-        StartCoroutine(DoBlink(1.5f));
+    [ContextMenu("Invisibility Switch")]
+    public void VisibilitySwitch() {
+        Transition(ref _isVisible, "_Visibility");
     }
-    public IEnumerator DoTransition(float _t, bool _forward)
-    {
-        float t = 0;
-        float dt = 1 / _t; //1/10
-
-        while (t < 1)
+    
+    private void Transition(ref bool state, string key) {
+        if (_canTransition)
         {
-            t += dt * Time.deltaTime;
-
-            if (_forward)
-                meshRenderer.sharedMaterial.SetFloat("_Transition", t);
-            else
-                meshRenderer.sharedMaterial.SetFloat("_Transition", 1-t);
-
+            StartCoroutine(MaterialValueLerp(state, key));
+            state = !state;
+        }
+    }
+    private IEnumerator MaterialValueLerp(bool state, string key)
+    {
+        _canTransition = false;
+        float elapsedTime = 0;
+       
+        int direction = state ? -1 : 1;
+        int finalValue = state ? 0 : 1;
+        
+        while (elapsedTime < 1) {
+            elapsedTime += Time.deltaTime;
+            _meshRenderer.sharedMaterial.SetFloat(key,
+                _meshRenderer.sharedMaterial.GetFloat(key) + Time.deltaTime * direction);
             yield return null;
         }
-
-        if (!_forward)
-        {
-            isToon = !isToon;
-            meshRenderer.sharedMaterial.SetFloat("_UseToon", isToon?1:0);
-        }
-    }
-    public IEnumerator DoBlink(float _t) 
-    {
-        float t = 0;
-        float dt = 1 / _t; //1/10
-
-        float max = 0.4f;
-        Color currentFresnel = meshRenderer.sharedMaterial.GetColor("_FresnelColor");        
-        Color.RGBToHSV(currentFresnel, out float hue, out float sat, out _);
-        while (t < 1)
-        {
-            t += dt * Time.deltaTime;
-            
-            Color newColor = Color.HSVToRGB(hue, sat, 1 - t);
-            meshRenderer.sharedMaterial.SetFloat("_refractionStrength", max * (1-t));
-            meshRenderer.sharedMaterial.SetColor("_FresnelColor", newColor);
-
-            yield return null;
-        }
-        t = 0;
-        while (t < 1)
-        {
-            t += dt * Time.deltaTime;
-            yield return null;
-        }
-
-        t = 0;
-        while (t < 1)
-        {
-            t += dt * Time.deltaTime;
-
-            Color newColor = Color.HSVToRGB(hue, sat, t);
-            meshRenderer.sharedMaterial.SetFloat("_refractionStrength", max * t);
-            meshRenderer.sharedMaterial.SetColor("_FresnelColor", newColor);
-
-            yield return null;
-        }
-
+        _meshRenderer.sharedMaterial.SetFloat(key, finalValue);
+        _canTransition = true;
     }
 }
